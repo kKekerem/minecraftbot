86197ee9f20a57cbfbe268e1a28943cf26c4a7a2
kKekerem <kerem300yt@gmail.com>
2025-10-18 19:52:33 +0300

    Add bot functionality to fakeplayers.js

---
+++ fakeplayers.js
@@ -0,0 +1,25 @@
+const mineflayer = require('mineflayer')
+function sleep(ms){return new Promise(r=>setTimeout(r,ms))}
+async function start(){
+  while(true){
+    const username='Fake_'+Math.floor(Math.random()*99999)
+    const bot=mineflayer.createBot({
+      host:'sunucu.ip.adresi', // sunucu IP'si buraya
+      port:25565,
+      username:username
+    })
+    bot.once('spawn',async()=>{
+      console.log(username+' girdi')
+      for(let i=0;i<10;i++){
+        bot.setControlState('forward',true)
+        await sleep(400)
+      }
+      bot.setControlState('forward',false)
+      await sleep(1500)
+      bot.quit()
+      console.log(username+' çıktı')
+    })
+    await sleep(60000) // 1dk
+  }
+}
+start()
